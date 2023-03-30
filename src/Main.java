59300cfa5774866752e2274fa9c3890c14da2225
he2794 <ehsani.fly@gmail.com>
2023-03-29 21:16:33 -0400

    lights added

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,11 @@ public class Main {
         X.size="1234cc";
         X.weight="6453k";
 
+        lights neon=new lights();
+        neon.color="red";
+        neon.size="15cm";
+        neon.intensity=" 7675lumin";
 
 
-        
     }
 }
